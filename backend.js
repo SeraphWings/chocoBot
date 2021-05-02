67fd18c37703ceba3acaae1879e90aca6c3703a9
choko <ro22113176@gmail.com>
2021-05-02 09:33:21 +0800

    nodejs-backend complete

---
+++ backend.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const hostName = 'localhost';
+const port = 8080;
+
+const server = http.createServer((req ,res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    fs.readFile('./index.html', null, function (error, html) {
+        if (error) {
+            res.writeHead(404);
+            res.write('File not found!');
+        } else {
+            res.write(html);
+        }
+        res.end();
+    });
+
+});
+
+server.listen(port, hostName, () => {
+    console.log(`server is listening on http:// ${hostName}:${port}.`);
+});
